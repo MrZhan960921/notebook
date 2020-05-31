e76d271c452f53aef8c4e6e4e8708f30ba9d5906
zcq960921 <zcq960921@gamil.com>
2020-05-31 16:40:59 +0800

    测试:发现用户服务信息

---
+++ content-center/src/main/java/com/zcq/TestController.java
@@ -0,0 +1,45 @@
+package com.zcq;
+
+
+import com.zcq.dao.content.ShareMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 测试：服务发现，证明内容中心总能找到用户中心
+     *
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("test2")
+    public List<ServiceInstance> getInstances() {
+        // 查询指定服务的所有实例的信息
+        // consul/eureka/zookeeper...
+        return this.discoveryClient.getInstances("user-center");
+    }
+
+
+}
